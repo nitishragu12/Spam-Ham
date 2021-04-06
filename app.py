10ef2f6eee9568c592de461ea60197019beabc4f
nitishragu12 <69626001+nitishragu12@users.noreply.github.com>
2021-04-06 19:31:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import joblib
+import sklearn
+model = joblib.load('Email_Class')
+st.title('Spam Ham Classifier')
+ip = st.text_input('Enter your message: ')
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
